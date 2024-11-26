dd3d57bd564aee10488bdb97dd7eece586641967
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-11-26 21:52:13 +0100

    setup bootstrap defaults

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,33 +1,42 @@
 import { NavLink } from "react-router-dom";
-import classes from './Navigation.module.scss'
+import classes from "./Navigation.module.scss";
 
 const Navigation = () => {
-    const activeLink = ({ isActive }) => isActive ? classes.isActive : undefined
+  const activeLink = ({ isActive }) =>
+    isActive ? classes.isActive : undefined;
 
-    return <header className="w-full bg-purple">
-        <nav>
-            <ul className="flex justify-center gap-5 p-5">
-                {/* <li>
-                    <NavLink
-                        to="/"
-                        className={activeLink}
-                        end
-                    >
-                        Home
-                    </NavLink>
-                </li> */}
-                <li>
-                    <NavLink to="/artikli" className={activeLink} end>Products</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/artikli/novi-artikal" className={activeLink} end>Novi Artikal</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/prijava" className={activeLink} end>Login</NavLink>
-                </li>
-            </ul>
-        </nav>
+  return (
+    <header className="w-full bg-purple">
+      <nav>
+        <ul className="d-flex gap-5 p-5">
+          {/* <li>
+                  <NavLink
+                      to="/"
+                      className={activeLink}
+                      end
+                  >
+                      Home
+                  </NavLink>
+              </li> */}
+          <li>
+            <NavLink to="/artikli" className={activeLink} end>
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/artikli/novi-artikal" className={activeLink} end>
+              Novi Artikal
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/prijava" className={activeLink} end>
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
     </header>
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
