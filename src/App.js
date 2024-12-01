49d83bdced7ad2ba02a2a891ba8f85c936d1f1e2
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-12-01 11:02:34 +0100

    hide router future warning messages

--- src/App.js
+++ src/App.js
@@ -8,24 +8,38 @@ import EditProductPage from "./pages/EditProductPage";
 import LoginPage from "./pages/LoginPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Wrapper />,
+      errorElement: <NotFoundPage />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/artikli", element: <Products /> },
+        { path: "/artikli/novi-artikal", element: <SaveProductPage /> },
+        { path: "/artikli/:id", element: <ProductPage /> },
+        { path: "/artikli/:id/promijeni", element: <EditProductPage /> },
+        { path: "/prijava", element: <LoginPage /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Wrapper />,
-    errorElement: <NotFoundPage />,
-    children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/artikli", element: <Products /> },
-      { path: "/artikli/novi-artikal", element: <SaveProductPage /> },
-      { path: "/artikli/:id", element: <ProductPage /> },
-      { path: "/artikli/:id/promijeni", element: <EditProductPage /> },
-      { path: "/prijava", element: <LoginPage /> },
-    ],
-  },
-]);
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+      v7_startTransition: true,
+    },
+  }
+);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  );
 }
 
 export default App;
