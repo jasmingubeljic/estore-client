161d0d2740a243a30b6b64d206ce1150e6c471bf
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-12-13 23:38:10 +0100

    card update

--- src/components/product/ProductCard.js
+++ src/components/product/ProductCard.js
@@ -4,8 +4,8 @@ import Col from "react-bootstrap/Col";
 import Stack from "react-bootstrap/Stack";
 import Badge from "react-bootstrap/Badge";
 import { apiUrl } from "../../appInfo";
-import styles from "./ProductCard.module.scss";
 import TimeAgo from "react-timeago";
+import styles from "./ProductCard.module.scss";
 
 const ProductCard = ({ product }) => {
   return (
@@ -18,13 +18,17 @@ const ProductCard = ({ product }) => {
             className={styles["image-sizing"]}
             // className="ratio ratio-1x1"
           />
-          <Card.Body className="p-2">
+          <Card.Body className="p-2 pb-0">
             <Stack gap={1}>
-              <Card.Title className="text-dark text-truncate">
+              <Card.Title
+                className={`text-dark text-truncate ${styles["card-title"]}`}
+              >
                 {product.title}
               </Card.Title>
               <Stack direction="horizontal" gap={1}>
-                <Card.Text className="my-auto text-info">
+                <Card.Text
+                  className={`my-auto text-info text-truncate ${styles["time-ago"]}`}
+                >
                   <TimeAgo date={product.createdAt} />
                 </Card.Text>
                 <Badge className="ms-auto my-auto py-2 px-2 bg-white text-info fs-6">
