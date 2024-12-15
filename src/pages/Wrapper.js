8673439fc3c29eb9fd2e3fac42e0678746910714
Jasmin Gubeljic <jasmin.g@tutanota.com>
2024-12-15 21:38:04 +0100

    style footer

--- src/pages/Wrapper.js
+++ src/pages/Wrapper.js
@@ -1,15 +1,15 @@
-import { Outlet } from 'react-router-dom'
-import Navigation from '../components/navigation/Navigation'
-import Footer from '../components/footer/Footer'
+import { Outlet } from "react-router-dom";
+import Navigation from "../components/navigation/Navigation";
+import Footer from "../components/footer/Footer";
 
 const Wrapper = () => {
-    return (
-        <div>
-            <Navigation />
-            <Outlet />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
-export default Wrapper
+export default Wrapper;
